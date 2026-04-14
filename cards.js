24d74f9fa6827695da28ec18d0182d2f7c451d61
breathiac <james.a.runciman@gmail.com>
2026-04-14 17:10:00 +0100

    DUMP
    Ready for testing

---
+++ cards.js
@@ -0,0 +1,310 @@
+window.CARDS = {
+    monsters: [
+        {
+            name: "Fire Elemental",
+            cost: 5,
+            copies: 3,
+            type: "effect"
+        },
+        {
+            name: "Stone Golem",
+            cost: 7,
+            copies: 2,
+            type: "normal"
+        },
+        {
+            name: "Abyss Drake",
+            cost: 9,
+            copies: 1,
+            type: "fusion"
+        },
+		{
+            name: "Ritual Monster #1",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+            name: "Ritual Monster #2",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+            name: "Ritual Monster #3",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+            name: "Ritual Monster #4",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+            name: "Ritual Monster #5",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+            name: "Effect Monster",
+            cost: 5,
+            copies: 3,
+            type: "effect"
+        },
+		{
+            name: "Dummy Card",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+            name: "Dummy Card",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+            name: "Dummy Card",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+            name: "Dummy Card",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+            name: "Dummy Card",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+            name: "Dummy Card",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+            name: "Dummy Card",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+            name: "Dummy Card",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+            name: "Dummy Card",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+            name: "Dummy Card",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        }
+    ],
+
+    spells: [
+        {
+            name: "Spell of Unbreakable Willpower",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        },
+		        {
+            name: "Dummy Card",
+            cost: 3,
+            copies: 5,
+            type: "spell"
+        }
+    ],
+
+    traps: [
+        {
+            name: "Pitfall Trap",
+            cost: 4,
+            copies: 3,
+            type: "trap"
+        },
+		        {
+            name: "Pitfall Trap",
+            cost: 4,
+            copies: 3,
+            type: "trap"
+        },
+		        {
+            name: "Pitfall Trap",
+            cost: 4,
+            copies: 3,
+            type: "trap"
+        },
+		        {
+            name: "Pitfall Trap",
+            cost: 4,
+            copies: 3,
+            type: "trap"
+        },
+		        {
+            name: "Pitfall Trap",
+            cost: 4,
+            copies: 3,
+            type: "trap"
+        },
+		        {
+            name: "Pitfall Trap",
+            cost: 4,
+            copies: 3,
+            type: "trap"
+        },
+		        {
+            name: "Pitfall Trap",
+            cost: 4,
+            copies: 3,
+            type: "trap"
+        },
+		        {
+            name: "Pitfall Trap",
+            cost: 4,
+            copies: 3,
+            type: "trap"
+        },
+		        {
+            name: "Pitfall Trap",
+            cost: 4,
+            copies: 3,
+            type: "trap"
+        },
+		        {
+            name: "Pitfall Trap",
+            cost: 4,
+            copies: 3,
+            type: "trap"
+        },
+		        {
+            name: "Pitfall Trap",
+            cost: 4,
+            copies: 3,
+            type: "trap"
+        },
+		        {
+            name: "Pitfall Trap",
+            cost: 4,
+            copies: 3,
+            type: "trap"
+        },
+		        {
+            name: "Pitfall Trap",
+            cost: 4,
+            copies: 3,
+            type: "trap"
+        }
+    ]
+};
