9282cdd0a18e725d6662dd48a1d65b8ddfe525d1
breathiac <james.a.runciman@gmail.com>
2026-04-14 22:17:18 +0100

    New things
    Added player colours
    Card tracking system

--- cards.js
+++ cards.js
@@ -1,114 +1,140 @@
 window.CARDS = {
     monsters: [
         {
+			id: "mon1",
             name: "Fire Elemental",
             cost: 5,
             copies: 3,
             type: "effect"
         },
         {
+			id: "mon2",
             name: "Stone Golem",
             cost: 7,
             copies: 2,
             type: "normal"
         },
         {
+			id: "mon3",
             name: "Abyss Drake",
             cost: 9,
             copies: 1,
             type: "fusion"
         },
 		{
+			id: "mon4",
             name: "Ritual Monster #1",
             cost: 5,
             copies: 3,
             type: "ritual"
         },
 		{
+			id: "mon5",
             name: "Ritual Monster #2",
             cost: 5,
             copies: 3,
             type: "ritual"
         },
 		{
+			id: "mon6",
             name: "Ritual Monster #3",
             cost: 5,
             copies: 3,
             type: "ritual"
         },
 		{
+			id: "mon7",
             name: "Ritual Monster #4",
             cost: 5,
             copies: 3,
             type: "ritual"
         },
 		{
+			id: "mon8",
             name: "Ritual Monster #5",
             cost: 5,
             copies: 3,
             type: "ritual"
         },
 		{
+			id: "mon9",
             name: "Effect Monster",
             cost: 5,
             copies: 3,
             type: "effect"
         },
 		{
+			id: "mon10",
+            name: "Bumhole The Sorceror",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+			id: "mon11",
+            name: "Dummy Card 3",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+			id: "mon12",
+            name: "Dummy Card 4",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+			id: "mon13",
+            name: "Dummy Card 5",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+			id: "mon14",
+            name: "Dummy Card 6",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+			id: "mon15",
+            name: "Dummy Card 7",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+			id: "mon16",
+            name: "Dummy Card 8",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+			id: "mon17",
+            name: "Dummy Card 9",
+            cost: 5,
+            copies: 3,
+            type: "ritual"
+        },
+		{
+			id: "mon18",
             name: "Dummy Card",
             cost: 5,
             copies: 3,
             type: "ritual"
         },
 		{
+			id: "mon19",
             name: "Dummy Card",
             cost: 5,
             copies: 3,
             type: "ritual"
         },
 		{
-            name: "Dummy Card",
-            cost: 5,
-            copies: 3,
-            type: "ritual"
-        },
-		{
-            name: "Dummy Card",
-            cost: 5,
-            copies: 3,
-            type: "ritual"
-        },
-		{
-            name: "Dummy Card",
-            cost: 5,
-            copies: 3,
-            type: "ritual"
-        },
-		{
-            name: "Dummy Card",
-            cost: 5,
-            copies: 3,
-            type: "ritual"
-        },
-		{
-            name: "Dummy Card",
-            cost: 5,
-            copies: 3,
-            type: "ritual"
-        },
-		{
-            name: "Dummy Card",
-            cost: 5,
-            copies: 3,
-            type: "ritual"
-        },
-		{
-            name: "Dummy Card",
-            cost: 5,
-            copies: 3,
-            type: "ritual"
-        },
-		{
+			id: "mon20",
             name: "Dummy Card",
             cost: 5,
             copies: 3,
@@ -118,108 +144,126 @@ window.CARDS = {
 
     spells: [
         {
+			id: "sp1",
             name: "Spell of Unbreakable Willpower",
             cost: 3,
             copies: 5,
             type: "spell"
+				},
+		{
+			id: "sp2",
+            name: "Spell 2",
+            cost: 3,
+            copies: 5,
+            type: "spell"
         },
 		        {
+			id: "sp3",
             name: "Dummy Card",
             cost: 3,
             copies: 5,
             type: "spell"
         },
 		        {
+			id: "sp4",
             name: "Dummy Card",
             cost: 3,
             copies: 5,
             type: "spell"
         },
 		        {
+			id: "sp5",
             name: "Dummy Card",
             cost: 3,
             copies: 5,
             type: "spell"
         },
 		        {
+			id: "sp6",
             name: "Dummy Card",
             cost: 3,
             copies: 5,
             type: "spell"
         },
 		        {
+			id: "sp7",
             name: "Dummy Card",
             cost: 3,
             copies: 5,
             type: "spell"
         },
 		        {
+			id: "sp8",
             name: "Dummy Card",
             cost: 3,
             copies: 5,
             type: "spell"
         },
 		        {
+			id: "sp9",
             name: "Dummy Card",
             cost: 3,
             copies: 5,
             type: "spell"
         },
 		        {
+			id: "sp10",
             name: "Dummy Card",
             cost: 3,
             copies: 5,
             type: "spell"
         },
 		        {
+			id: "sp11",
             name: "Dummy Card",
             cost: 3,
             copies: 5,
             type: "spell"
         },
 		        {
+			id: "sp12",
             name: "Dummy Card",
             cost: 3,
             copies: 5,
             type: "spell"
         },
 		        {
+			id: "sp13",
             name: "Dummy Card",
             cost: 3,
             copies: 5,
             type: "spell"
         },
 		        {
+			id: "sp14",
             name: "Dummy Card",
             cost: 3,
             copies: 5,
             type: "spell"
         },
 		        {
+			id: "sp15",
             name: "Dummy Card",
             cost: 3,
             copies: 5,
             type: "spell"
         },
 		        {
+			id: "sp16",
             name: "Dummy Card",
             cost: 3,
             copies: 5,
             type: "spell"
         },
 		        {
+			id: "sp17",
             name: "Dummy Card",
             cost: 3,
             copies: 5,
             type: "spell"
         },
 		        {
-            name: "Dummy Card",
-            cost: 3,
-            copies: 5,
-            type: "spell"
-        },
-		        {
+			id: "sp18",
             name: "Dummy Card",
             cost: 3,
             copies: 5,
@@ -229,78 +273,91 @@ window.CARDS = {
 
     traps: [
         {
+			id: "t1",
             name: "Pitfall Trap",
             cost: 4,
             copies: 3,
             type: "trap"
         },
 		        {
+			id: "t2",
             name: "Pitfall Trap",
             cost: 4,
             copies: 3,
             type: "trap"
         },
 		        {
+			id: "t3",
             name: "Pitfall Trap",
             cost: 4,
             copies: 3,
             type: "trap"
         },
 		        {
+			id: "t4",
             name: "Pitfall Trap",
             cost: 4,
             copies: 3,
             type: "trap"
         },
 		        {
+			id: "t5",
             name: "Pitfall Trap",
             cost: 4,
             copies: 3,
             type: "trap"
         },
 		        {
+			id: "t6",
             name: "Pitfall Trap",
             cost: 4,
             copies: 3,
             type: "trap"
         },
 		        {
+			id: "t7",
             name: "Pitfall Trap",
             cost: 4,
             copies: 3,
             type: "trap"
         },
 		        {
+			id: "t8",
             name: "Pitfall Trap",
             cost: 4,
             copies: 3,
             type: "trap"
         },
 		        {
+			id: "t9",
             name: "Pitfall Trap",
             cost: 4,
             copies: 3,
             type: "trap"
         },
 		        {
+			id: "t10",
             name: "Pitfall Trap",
             cost: 4,
             copies: 3,
             type: "trap"
         },
 		        {
+			id: "t11",
             name: "Pitfall Trap",
             cost: 4,
             copies: 3,
             type: "trap"
         },
 		        {
+			id: "t12",
             name: "Pitfall Trap",
             cost: 4,
             copies: 3,
             type: "trap"
         },
 		        {
+			id: "t13",
             name: "Pitfall Trap",
             cost: 4,
             copies: 3,
